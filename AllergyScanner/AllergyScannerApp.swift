f0b61d696ad92a9bf1db81de45249716def5bc4f
Ahmed Dizdar <ahmed.dizdar@deutschebahn.com>
2023-05-06 12:32:59 +0200

    Initial Commit

---
+++ AllergyScanner/AllergyScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyScannerApp.swift
+//  AllergyScanner
+//
+//  Created by Ahmed Dizdar on 06.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AllergyScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
