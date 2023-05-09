94602f30c296c810123b2eceeb3153094d196919
Ahmed Dizdar <ahmed.dizdar@deutschebahn.com>
2023-05-09 19:48:42 +0200

    Add new text editor design

--- AllergyScanner/Presentation/Views/IngridientTextEditor.swift
+++ AllergyScanner/Presentation/Views/IngridientTextEditor.swift
@@ -20,7 +20,6 @@ struct IngridientTextEditor: View {
 		VStack {
 			HStack {
 				ClearAllButton(operation: clearOperation)
-					.padding(.horizontal)
 
 				Button {
 					showingFileImporter = true
@@ -46,8 +45,20 @@ struct IngridientTextEditor: View {
 				.focused($hasFocused)
 				.sync($bindableHasFocues, with: _hasFocused)
 				.font(.body)
-				.padding(.all)
 				.cornerRadius(10)
+				.foregroundColor(Color.primary)
+				.padding(4)
+				.background(
+					RoundedRectangle(cornerRadius: 10)
+						.fill(Color.white)
+						.shadow(
+							color: hasFocused ? .blue.opacity(0.5) : .black.opacity(0.2),
+							radius: 5,
+							x: 0,
+							y: 2
+						)
+				)
+
 		}
 	}
 }
